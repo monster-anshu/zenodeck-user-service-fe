a5134569af02e1cf1c265ba2e68adc846b1aa537
Himanshu Gunwant <hgunwant2312@gmail.com>
2024-09-24 11:12:40 +0530

    add redirect

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,11 @@
+import { redirect } from '@remix-run/react';
 import { FC } from 'react';
 
 type IHomeProps = {};
+
+export const loader = () => {
+  return redirect('/user');
+};
 
 const Home: FC<IHomeProps> = () => {
   return <div>Home</div>;
