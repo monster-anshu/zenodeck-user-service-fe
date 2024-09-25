20ee868e8393a4cc7e1704bbbcc6a32568dd0a95
Himanshu Gunwant <hgunwant2312@gmail.com>
2024-09-25 14:14:47 +0530

    add theme

--- app/root.tsx
+++ app/root.tsx
@@ -4,17 +4,38 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from '@remix-run/react';
 import './tailwind.css';
 import ReactQueryProvider from './providers/react-query';
 import { Toaster } from 'sonner';
+import {
+  NonFlashOfWrongThemeEls,
+  ThemeProvider,
+  useTheme,
+} from './providers/theme';
+import { cn } from './lib/utils';
+import { getThemeSession } from './utils/theme.server';
+import { LoaderFunctionArgs } from '@remix-run/node';
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const themeSession = await getThemeSession(request);
+
+  return {
+    theme: themeSession.getTheme(),
+  };
+};
+
+const Root = ({ children }: { children: React.ReactNode }) => {
+  const [theme] = useTheme();
+  const data = useLoaderData<typeof loader>();
+
   return (
-    <html lang='en' className=''>
+    <html lang='en' className={cn(theme)}>
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <NonFlashOfWrongThemeEls ssrTheme={false && Boolean(data.theme)} />
         <Meta />
         <Links />
       </head>
@@ -25,6 +46,15 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Toaster />
       </body>
     </html>
+  );
+};
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  const data = useLoaderData<typeof loader>();
+  return (
+    <ThemeProvider specifiedTheme={data.theme}>
+      <Root>{children}</Root>
+    </ThemeProvider>
   );
 }
 
---
+++ app/utils/theme.server.ts
@@ -0,0 +1,26 @@
+import { createCookieSessionStorage } from '@remix-run/node';
+import { isTheme, Theme } from '~/providers/theme';
+
+const themeStorage = createCookieSessionStorage({
+  cookie: {
+    name: 'theme',
+    secure: true,
+    sameSite: 'lax',
+    path: '/',
+    httpOnly: true,
+  },
+});
+
+async function getThemeSession(request: Request) {
+  const session = await themeStorage.getSession(request.headers.get('Cookie'));
+  return {
+    getTheme: () => {
+      const themeValue = session.get('theme');
+      return isTheme(themeValue) ? themeValue : null;
+    },
+    setTheme: (theme: Theme) => session.set('theme', theme),
+    commit: () => themeStorage.commitSession(session),
+  };
+}
+
+export { getThemeSession };
